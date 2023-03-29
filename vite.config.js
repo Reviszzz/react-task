8ee739f8b6a981fbb92027ed7ea178eecf9fbb49
JTRUJILLOCRUZ <96313226+JTRUJILLOCRUZ@users.noreply.github.com>
2023-03-29 17:20:19 -0500

    base uptade

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Reviszzz.github.io/react-task'
+  base: 'https://Reviszzz.github.io/react-task/'
 })
