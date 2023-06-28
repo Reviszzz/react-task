5c9a087386e160c8e92d7d83a47e753216e7df3e
JTRUJILLOCRUZ <96313226+JTRUJILLOCRUZ@users.noreply.github.com>
2023-06-28 15:46:02 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,11 @@ import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
 
 function App() {
-
-
-
-  
-
   return (
     <main className="bg-zinc-900 h-screen">
-      <div className="container mx-auto p-10">
-      <TaskForm />
-      <TaskList />
+      <div className="bg-zinc-900 container mx-auto p-10 min-w-full">
+        <TaskForm />
+        <TaskList />
       </div>
     </main>
   );
